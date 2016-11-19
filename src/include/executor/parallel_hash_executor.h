5b692a4fe0bf49e21a37857217cf6e35f9495927
eric-haibin-lin <linhaibin.eric@gmail.com>
2016-11-18 22:03:18 -0500

    Make a copy of hash join files #3

---
+++ src/include/executor/parallel_hash_executor.h
@@ -0,0 +1,76 @@
+//===----------------------------------------------------------------------===//
+//
+//                         Peloton
+//
+// parallel_hash_executor.h
+//
+// Identification: src/include/executor/parallel_hash_executor.h
+//
+// Copyright (c) 2015-16, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include <unordered_map>
+#include <unordered_set>
+
+#include "common/types.h"
+#include "executor/abstract_executor.h"
+#include "executor/logical_tile.h"
+#include "common/container_tuple.h"
+
+#include <boost/functional/hash.hpp>
+
+namespace peloton {
+namespace executor {
+
+/**
+ * @brief Hash executor.
+ *
+ */
+class ParallelHashExecutor : public AbstractExecutor {
+ public:
+  ParallelHashExecutor(const ParallelHashExecutor &) = delete;
+  ParallelHashExecutor &operator=(const ParallelHashExecutor &) = delete;
+  ParallelHashExecutor(const ParallelHashExecutor &&) = delete;
+  ParallelHashExecutor &operator=(const ParallelHashExecutor &&) = delete;
+
+  explicit ParallelHashExecutor(const planner::AbstractPlan *node,
+                                ExecutorContext *executor_context);
+
+  /** @brief Type definitions for hash table */
+  typedef std::unordered_map<
+      expression::ContainerTuple<LogicalTile>,
+      std::unordered_set<std::pair<size_t, oid_t>,
+                         boost::hash<std::pair<size_t, oid_t>>>,
+      expression::ContainerTupleHasher<LogicalTile>,
+      expression::ContainerTupleComparator<LogicalTile>> HashMapType;
+
+  inline HashMapType &GetHashTable() { return this->hash_table_; }
+
+  inline const std::vector<oid_t> &GetHashKeyIds() const {
+    return this->column_ids_;
+  }
+
+ protected:
+  bool DInit();
+
+  bool DExecute();
+
+ private:
+  /** @brief Hash table */
+  HashMapType hash_table_;
+
+  /** @brief Input tiles from child node */
+  std::vector<std::unique_ptr<LogicalTile>> child_tiles_;
+
+  std::vector<oid_t> column_ids_;
+
+  bool done_ = false;
+
+  size_t result_itr = 0;
+};
+
+} /* namespace executor */
+} /* namespace peloton */
---
+++ src/include/executor/parallel_hash_join_executor.h
@@ -0,0 +1,53 @@
+//===----------------------------------------------------------------------===//
+//
+//                         Peloton
+//
+// hash_join_executor.h
+//
+// Identification: src/include/executor/hash_join_executor.h
+//
+// Copyright (c) 2015-16, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include <deque>
+#include <vector>
+
+#include "executor/abstract_join_executor.h"
+#include "planner/hash_join_plan.h"
+#include "executor/parallel_hash_executor.h"
+
+namespace peloton {
+namespace executor {
+
+class ParallelHashJoinExecutor : public AbstractJoinExecutor {
+  ParallelHashJoinExecutor(const ParallelHashJoinExecutor &) = delete;
+  ParallelHashJoinExecutor &operator=(const ParallelHashJoinExecutor &) =
+      delete;
+
+ public:
+  explicit ParallelHashJoinExecutor(const planner::AbstractPlan *node,
+                                    ExecutorContext *executor_context);
+
+ protected:
+  bool DInit();
+
+  bool DExecute();
+
+ private:
+  ParallelHashExecutor *hash_executor_ = nullptr;
+
+  bool hashed_ = false;
+
+  std::deque<LogicalTile *> buffered_output_tiles;
+  std::vector<std::unique_ptr<LogicalTile>> right_tiles_;
+
+  // logical tile iterators
+  size_t left_logical_tile_itr_ = 0;
+  size_t right_logical_tile_itr_ = 0;
+};
+
+}  // namespace executor
+}  // namespace peloton
