cdf3f15a68d788cbddc41b46dbc0ab172edb40c1
eric-haibin-lin <linhaibin.eric@gmail.com>
2016-12-03 21:32:14 -0500

    Multi-thread test for hashmap

--- test/executor/hashmap_test.cpp
+++ test/executor/hashmap_test.cpp
@@ -25,7 +25,7 @@
 #include "executor/logical_tile_factory.h"
 #include "storage/data_table.h"
 
-#define NUM_ITERATION 5000000
+#define NUM_ITERATION 5000
 
 namespace peloton {
 namespace test {
@@ -35,27 +35,27 @@ class HashmapTests : public PelotonTest {};
 namespace {
 
 typedef executor::Hashmap<
-    expression::ContainerTuple<executor::LogicalTile>,               // Key
-    std::shared_ptr<executor::ParallelHashExecutor::ConcurrentSet>,  // T
-    expression::ContainerTupleHasher<executor::LogicalTile>,         // Hash
-    expression::ContainerTupleComparator<executor::LogicalTile>,     // Pred
+    int,                                                                // Key
+    std::shared_ptr<executor::ParallelHashExecutor::ConcurrentVector>,  // T
+    std::hash<int>,                                                     // Hash
+    std::equal_to<int>,                                                 // Pred
     4,  // Bucket size
     1   // Probe step size
-    > HashMapType;
-
-typedef executor::Hashmap<int,                 // Key
-                          int,                 // T
-                          std::hash<int>,      // Hash
-                          std::equal_to<int>,  // Pred
-                          4,                   // Bucket size
-                          1                    // Probe step size
-                          > TestHashMapType;
+    > TestHashMapType;
 
 void PutTest(TestHashMapType *hashmap, int num_keys,
              UNUSED_ATTRIBUTE uint64_t thread_itr) {
   for (oid_t itr = 0; itr < NUM_ITERATION; itr++) {
     for (int k = 0; k < num_keys; k++) {
-      hashmap->Put(k, k);
+      std::shared_ptr<executor::ParallelHashExecutor::ConcurrentVector> val;
+      bool success = hashmap->Get(k, val);
+      if (success == false) {
+        val.reset(new executor::ParallelHashExecutor::ConcurrentVector());
+        hashmap->Put(k, val);
+        success = hashmap->Get(k, val);
+        EXPECT_TRUE(success);
+      }
+      val->Insert(std::make_tuple(1, 1, 1));
     }
   }
 }
@@ -66,19 +66,41 @@ TEST_F(HashmapTests, BasicTest) {
   TestHashMapType hashmap;
   hashmap.Reserve(num_keys * 2);
 
+  std::shared_ptr<executor::ParallelHashExecutor::ConcurrentVector> val1(
+      new executor::ParallelHashExecutor::ConcurrentVector());
+  val1->Insert(std::make_tuple(1, 1, 1));
   int key1 = 1;
-  int val1 = 1;
-  int val1_result = 0;
-  hashmap.Put(key1, val1);
-  hashmap.Get(key1, val1_result);
-  EXPECT_EQ(val1, val1_result);
-  size_t num_threads = 4;
+  std::shared_ptr<executor::ParallelHashExecutor::ConcurrentVector> result;
+  bool success = hashmap.Put(key1, val1);
+  EXPECT_TRUE(success);
 
+  // Get existing key should succeed
+  success = hashmap.Get(key1, result);
+  EXPECT_TRUE(success);
+
+  EXPECT_EQ(val1->GetVector().size(), result->GetVector().size());
+
+  // Get non-existing key should fail
+  int key2 = 2;
+  success = hashmap.Get(key2, result);
+  EXPECT_FALSE(success);
+}
+
+// Insert to hash map in parallel
+TEST_F(HashmapTests, MultiThreadTest) {
+  int num_keys = 4;
+  TestHashMapType hashmap;
+  hashmap.Reserve(num_keys * 2);
+
+  size_t num_threads = 8;
   LaunchParallelTest(num_threads, PutTest, &hashmap, num_keys);
-  int v = 0;
+
+  // Validate the number of elements for each key
+  std::shared_ptr<executor::ParallelHashExecutor::ConcurrentVector> v;
   for (int k = 0; k < num_keys; k++) {
-    hashmap.Get(k, v);
-    EXPECT_EQ(k, v);
+    bool success = hashmap.Get(k, v);
+    EXPECT_TRUE(success);
+    EXPECT_EQ(v->GetVector().size(), num_threads * NUM_ITERATION);
   }
 }
 }
